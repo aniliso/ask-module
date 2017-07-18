1303d08db7a4e1495c66838ed4a7f37bf31e30c5
aniliso <tkaptan41@gmail.com>
2017-07-19 01:11:48 +0300

    Added russian language

---
+++ Resources/lang/ru/questions.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'list resource'    => 'Список вопросов',
+    'create resource'  => 'Добавить вопрос',
+    'edit resource'    => 'Изменить вопросы',
+    'destroy resource' => 'четкие вопросы',
+    'title'            => [
+        'questions'       => 'Вопросов',
+        'create question' => 'Создать вопрос',
+        'edit question'   => 'Изменить вопрос',
+    ],
+    'button'           => [
+        'create question' => 'Создать вопрос',
+    ],
+    'table'            => [
+        'fullname'     => 'Имя Фамилия',
+        'answered'     => 'Ответил',
+        'not answered' => 'Не отвечено'
+    ],
+    'form'             => [
+        'first_name'  => 'имя',
+        'last_name'   => 'Фамилия',
+        'email'       => 'Электронная почта',
+        'phone'       => 'телефон',
+        'question'    => 'Вопрос',
+        'answer'      => 'Ответ',
+        'captcha_ask' => 'Код безопасности',
+        'attachment'  => 'Добавить файл',
+        'is_answered' => 'Ответ',
+    ],
+    'messages'         => [
+        'file not selected' => 'Нет выбора файла',
+        'success message'   => 'Ваш вопрос успешно добавлен. Мы ответим как можно скорее. Спасибо.',
+        'error message'     => 'Возникла ошибка вашего вопроса. Пожалуйста, попробуйте еще раз.',
+    ],
+    'validation'       => [
+    ],
+];
